5a9e885aaa6b51685090e56cbad4e7ac314b60e2
pmbfsa <paulomarcioba@gmail.com>
2026-05-22 00:58:04 -0300

    Fetch an array of quotes from Jacinto Design API when loading the page

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let apiQuotes = [];
 
+// Show new quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+  try {
+    const response = await fetch(apiUrl);
+
+    // Confirm HTTP response
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status}.\nUsing local quotes list.`);
+    }
+
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(error);
+    apiQuotes = localQuotes;
+    newQuote();
+  }
+}
+
+// On Load
+getQuotes();
